68586815650e7473920ef5211a59eaf2919ec483
Kalash022 <aniketasa30@gmail.com>
2024-05-03 17:10:49 +0530

    leave server done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,7 +69,9 @@ const { onOpen } = useModal();
                 )}
 
                 {!isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                    onClick={()=> onOpen("leaveServer", {server})}
+                    className="px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOutIcon className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
